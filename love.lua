9baa3e87d0de78e52d763ba025e5ed1bda446708
sz1o <techchecker225@gmail.com>
2026-08-26 18:59:46 -0500

    mwa

--- love.lua
+++ love.lua
@@ -2,6 +2,6 @@ local _1 = string.char(99, 97, 116, 119, 97, 114, 101)
 local _2 = string.char(32)
 local _3 = string.reverse("u sevol")
 
-for i = 1, 5000 do
+for i = 1, 6000 do
     print(_1 .. _2 .. _3)
 end
